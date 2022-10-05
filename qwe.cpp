424fcc82c30aae18a1688116c3bf8561e4bdd16e
Karagezyan Slavik <slavik.karagezyan19@gmail.com>
2022-10-05 20:05:43 +0400

    first

---
+++ qwe.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+int main(){
+	std::cout << "5" <<
+		std::cout << "5" << std::endl;
+
+
+	std::cout << "5" << sklsankltd::endl;
+	return 0;std::cout << "5" << std::endl;
+
+
+	std::cout << "5" << std::endl;
+	return 0;std::cout << "5" << std::endl;
+
+
+	std::cout << "5" << std::endl;
+	return 0;std::cout << "5" << std::endl;
+
+
+	std::cout << "5" << std::endl;
+	return 0;std::cout << "5" << std::endl;
+
+
+	std::cout << "5" << std::endl;
+	return 0;std::cout << "5" << std::endl;
+
+
+	std::cout << "5" << std::endl;
+	return 0;std::cout << "5" << std::endl;
+
+
+	std::cout << "5" << std::endl;
+	return 0;std::cout << "5" << std::endl;
+
+
+	std::cout << "5" << std::endl;
+	return 0;std::cout << "5" << std::endl;
+
+
+	std::cout << "5" << std::endl;
+	return 0;std::cout << "5" << std::endl;
+
+
+	std::cout << "5" << std::endl;
+	return 0; std::endl;
+
+
+	std::cout << "5" << std::endl;
+	return 0;
+
+	std::cout << "5" << std::endl;
+
+
+	std::cout << "5" << std::endl;
+	return 0;
+	for(int i = 0; i < n; i++){
+		
+	}
+
+}
+
